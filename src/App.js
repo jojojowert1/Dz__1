218e21c3edd57366602f7501ca8131bf660b1670
Emir <jojojo5766@gmail.com>
2024-03-09 16:35:07 +0600

    dz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Title = ({ text }) =>{
+    return <h1>{text}</h1>;
+}
+const Header = () => {
+    return (
+        <div>
+            <Title text="Header text"/>
+        </div>
+    )
 }
 
-export default App;
+const Content = ()=>{
+    return(
+        <div>
+            <Title text="Content text" />
+        </div>
+    )
+}
+const Footer = ()=>{
+    return(
+        <div>
+            <Title text="Footer text" />
+        </div>
+    )
+}
+
+
+function App() {
+    const [value, setValue] = useState(0)
+    const [run, setRun] = useState(false)
+
+    useEffect(() => {
+        // let intervalId
+        if (run) {
+             setInterval(() => {
+                setValue((count) => count +1)
+            },1000)
+        }
+        // return() => clearInterval(intervalId)
+    }, [run])
+    const start = () => {
+        setRun(true)
+    }
+    const stop = () => {
+        setRun(false)
+    }
+    const reset = () => {
+        setValue(0)
+        setRun(false)
+    }
+  return (
+      <div>
+          <h1>{value}</h1>
+          <button onClick={start}>start</button>
+          <button onClick={stop}>stop</button>
+          <button onClick={reset}>reset</button>
+          <Header/>
+          <Content/>
+          <Footer/>
+      </div>
+      )
+
+}
+export default App
+
+
+
